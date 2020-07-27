cc1a8a322da81142fcdc4bd62ea7eb87b0de9fca
Rahul Gururaj <rahul.gururaj96@gmail.com>
2020-07-27 16:24:06 +1000

    Added a home page and firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD4aEV4XBuGHb-hthsvEZccXnSNictkzjY",
+    authDomain: "kanban-d68ed.firebaseapp.com",
+    databaseURL: "https://kanban-d68ed.firebaseio.com",
+    projectId: "kanban-d68ed",
+    storageBucket: "kanban-d68ed.appspot.com",
+    messagingSenderId: "715128617389",
+    appId: "1:715128617389:web:a9cc52e1713e6d5a16c924",
+    measurementId: "G-9JRTLWQL27"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyD4aEV4XBuGHb-hthsvEZccXnSNictkzjY",
+    authDomain: "kanban-d68ed.firebaseapp.com",
+    databaseURL: "https://kanban-d68ed.firebaseio.com",
+    projectId: "kanban-d68ed",
+    storageBucket: "kanban-d68ed.appspot.com",
+    messagingSenderId: "715128617389",
+    appId: "1:715128617389:web:a9cc52e1713e6d5a16c924",
+    measurementId: "G-9JRTLWQL27"
+  }
 };
 
 /*
